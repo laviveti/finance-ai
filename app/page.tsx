e044d6302bd9ead3cfae18eabc291816dec8703c
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-11-14 11:43:11 -0300

    chore(0h51): add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <h1 className="text-red-500">Hello world!</h1>
+      <Button>Hello world!</Button>
     </div>
   );
 }
